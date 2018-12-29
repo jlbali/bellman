4368ac6160d7d29c46816e4787e53e87c632075e
lbali <juan.lucas.bali@gmail.com>
2018-12-28 21:06:13 -0300

    More experiments with the Barrier Solver

--- Numerical.py
+++ Numerical.py
@@ -8,7 +8,7 @@ Created on Fri Dec 28 19:20:58 2018
 
 import numpy as np
 from scipy.optimize import minimize
-
+import sys
 
 
 class BarrierSolver:
@@ -21,9 +21,11 @@ class BarrierSolver:
         self.mu = mu
         
     
-    def solve(self, stop_criteria, x0, t0):
+    def slow_solve(self, stop_criteria, x0, t0):
         t = t0
         xk = x0
+        traj = []
+        grad_traj = []
         while t <= stop_criteria:
             #print("t: ", t)
             #print("xk: ", xk)
@@ -33,11 +35,50 @@ class BarrierSolver:
             #res = minimize(opt_fun, xk, method="BFGS", options={'gtol': 1e-05, 'norm': np.inf, 'eps': 1.4901161193847656e-20, 'maxiter': None, 'disp': display, 'return_all': False})
             #res = minimize(opt_fun, xk, method="SLSQP", options={'eps': 1.4901161193847656e-10,  'disp': display})
             #res = minimize(opt_fun, xk, method="BFGS", options={'gtol': 1e-20, 'norm': np.inf, 'maxiter': None, 'disp': display, 'return_all': False})
-            res = minimize(opt_fun, xk)
-            xk = res.x
+            #res = minimize(opt_fun, xk)
+            grad_eps = 1.0e-4 # Feo, con valores altos se queda en el x0 como optimo, con valores bajos se va completamente de rango, muy raro, es casi al reves que antes.
+            while True:
+                #res = minimize(opt_fun, xk, method="BFGS", options={'gtol': 1e-05, 'norm': np.inf, 'eps': grad_eps, 'maxiter': None, 'disp': display, 'return_all': False})
+                res = minimize(opt_fun, xk, method="SLSQP", options={'eps': grad_eps,  'disp': display})
+                x = res.x
+                y = opt_fun(x)
+                #print("y" , y)
+                #print("xk" , xk)
+                #print("grad_eps: ", grad_eps)
+                if not np.isnan(y) and not np.isinf(y):
+                    xk = x
+                    break
+                grad_eps = grad_eps / 2.0
+                if grad_eps == 0.0:
+                    sys.exit(1)
+            traj.append(xk)
+            grad_traj.append(grad_eps)
             t = t*self.mu
+        #print("Traj: ", traj)
+        #print("grad_traj: ", grad_traj)
         return xk
         
+    def solve(self, stop_criteria, x0, t0):
+        t = t0
+        xk = x0
+        traj = []
+        print("Initial: ", x0)
+        while t <= stop_criteria:
+            #print("t: ", t)
+            #print("xk: ", xk)
+            def opt_fun(x):
+                return t*self.f(x) + self.phi(x)
+            display = False
+            #res = minimize(opt_fun, xk, method="BFGS", options={'gtol': 1e-05, 'norm': np.inf, 'eps': 1.4901161193847656e-20, 'maxiter': None, 'disp': display, 'return_all': False})
+            res = minimize(opt_fun, xk, method="SLSQP", options={'eps': 1.4901161193847656e-18,  'disp': display})
+            #res = minimize(opt_fun, xk, method="BFGS", options={'gtol': 1e-20, 'norm': np.inf, 'maxiter': None, 'disp': display, 'return_all': False})
+            #res = minimize(opt_fun, xk)
+            xk = res.x
+            print("xk: ", xk)
+            traj.append(xk)
+            t = t*self.mu
+        print("Traj: ", traj)
+        return xk
 
 
 
--- OptimalGrowth.py
+++ OptimalGrowth.py
@@ -227,10 +227,12 @@ class OptimalGrowth:
                     return -(np.log(y) + np.log(self.f(x) - y))
                 solver = BarrierSolver(f, phi, mu)                
                 y0 = (0 + self.f(x))/2
-                y_opt = solver.solve(stop_criteria, [y0], t0)[0]
+                y_opt = solver.slow_solve(stop_criteria, np.array([y0]), t0)[0]
                 g_values[i] = y_opt
                 #g_values[i] = y0 # Se queda con el y0!!! y_opt = y_0
                 V_new[i] = self.U(x,y_opt) + self.beta*V(y_opt)
+                print("y0: ", y0)
+                print("y_opt: ", y_opt) # COINCIDEN!!!
                 if np.isnan(V(y_opt)) or np.isnan(self.U(x,y_opt)):
                     print("NAN detected!")
                     sys.exit()
